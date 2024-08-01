2fdc37e2044953383d7a4552e086247b9524a30c
admin <malzejnullahu939@gmail.com>
2024-08-02 01:34:10 +0200

    linked to previous push

---
+++ Life-Ecommerce/TokenServiceDelete/AuthMiddlewareSoftDelete.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Life_Ecommerce.TokenService
+{
+    public class AuthMiddlewareSoftDelete
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddlewareSoftDelete(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("OUR_SECRET_KEY_FROM_LIFE_FROM_GJIRAFA");
+                try
+                {
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
+                    }, out SecurityToken validatedToken);
+
+                    var jwtToken = (JwtSecurityToken)validatedToken;
+                    var role = jwtToken.Claims.First(claim => claim.Type == "role").Value;
+
+                    context.Items["UserRole"] = role;
+                    context.Items["UserId"] = jwtToken.Claims.First(claim => claim.Type == "nameid").Value;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Authentication failed: {ex.Message}");
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync("Unauthorized");
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
